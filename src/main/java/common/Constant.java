fd2c48fb9ef69cfc7592b1886af45c59fc568e69
Baltan <617640006@qq.com>
2019-06-06 16:30:55 +0800

    在映射配置文件中引用静态常量和静态方法

---
+++ src/main/java/common/Constant.java
@@ -0,0 +1,15 @@
+package common;
+
+/**
+ * Description:
+ *
+ * @author Baltan
+ * @date 2019-06-06 15:34
+ */
+public class Constant {
+    public static final int COURSE_ID = 2;
+
+    public static int getCourseId() {
+        return 2;
+    }
+}
